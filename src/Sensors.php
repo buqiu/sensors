32ee182c25c72b6d3f6b37c33c76cbc3292ba74a
董奎 <466135132@qq.com>
2024-07-08 13:46:25 +0800

    增加自定义日志上报目录

--- src/Sensors.php
+++ src/Sensors.php
@@ -4,27 +4,24 @@ namespace Buqiu\Sensors;
 use Illuminate\Support\Facades\Log;
 class Sensors
 {
-    public string $user_uuid;
-
+    // 上报用户uuid
+    public string $userUUID;
     // 事件名
     public string $event;
-
     // 自定义属性
     public array $property;
-
-    // 行为
+    // 用户行为
     public string $action;
     // 身份属性
     public array $identityList;
     // 公共属性
     public array $common;
-
+    // 上传路径
     public string $logPath;
 
-    public function __construct(string $action, array $identityList, array $property = [], array $common = [], string $event = '')
+    public function __construct(string $action = '', array $identityList = [], array $property = [], array $common = [], string $event = '', string $logPath = '')
     {
-        $this->init($action, $identityList, $property, $common, $event);
-        $this->logPath = config('sensors.log_path').'/sensors.'.date('Y-m-d').'.log';
+        $this->init(action: $action,identityList:  $identityList,property:  $property,common:  $common, event: $event,logPath: $logPath);
     }
 
     /**
@@ -39,16 +36,16 @@ class Sensors
             $sdk->registerSuperProperties($this->common); // 注册公共属性
         }
         try {
-             match ($this->action) {
-                'profileSet'       => $sdk->profileSetById($this->user_uuid, $this->property), // 属性设置
-                'profileSetOnce'   => $sdk->profileSetOnceById($this->user_uuid, $this->property), // 属性初始化
-                'profileIncrement' => $sdk->profileIncrementById($this->user_uuid, $this->property), // 属性自增
-                'profileAppend'    => $sdk->profileAppendById($this->user_uuid, $this->property), // 追加属性
-                'bind'             => $sdk->bind($this->user_uuid, $this->property), // 绑定
+            match ($this->action) {
+                'profileSet'       => $sdk->profileSetById($this->userUUID, $this->property), // 属性设置
+                'profileSetOnce'   => $sdk->profileSetOnceById($this->userUUID, $this->property), // 属性初始化
+                'profileIncrement' => $sdk->profileIncrementById($this->userUUID, $this->property), // 属性自增
+                'profileAppend'    => $sdk->profileAppendById($this->userUUID, $this->property), // 追加属性
+                'bind'             => $sdk->bind($this->userUUID, $this->property), // 绑定
                 'unbind'           => $sdk->unbind($this->property), // 解绑
                 default            => $sdk->trackById($this->identityList, $this->event, $this->property), // track 事件记录上报
             };
-             return true;
+            return true;
         } catch (\SensorsAnalyticsIllegalDataException $e) {
             Log::channel('exception')->error('神策上报异常:', ['code' => $e->getCode(), 'file' => $e->getFile(), 'line' => $e->getLine(), 'message' => $e->getMessage()]);
 
@@ -59,31 +56,50 @@ class Sensors
     /**
      * 初始化.
      * @note init
-     * @param $action
-     * @param $identityList
-     * @param $property
-     * @param $common
-     * @param $event
-     * @return bool
+     * @param string $action
+     * @param array $identityList
+     * @param array $property
+     * @param array $common
+     * @param string $event
+     * @param string $logPath
      * @author caoruidong
      */
-    public function init($action, $identityList, $property, $common, $event): bool
+    public function init(string $action = '', array $identityList =[], array $property =[], array $common =[], string $event ='',string $logPath =''): void
     {
         $this->action    = $action;
         $this->event     = $event;
         $this->property  = $property;
-        $this->user_uuid = $identityList['user_uuid'];
+        $this->userUUID  = $identityList['user_uuid'] ?? null;
         $this->common    = $common;
-        if (isset($identityList['user_uuid']) && $identityList['user_uuid']) {
-            $this->identityList['$identity_login_id'] = $identityList['user_uuid'];
-        }
-        if (isset($identityList['identity_mp_unionid']) && $identityList['identity_mp_unionid']) {
-            $this->identityList['$identity_mp_unionid'] = $identityList['identity_mp_unionid'];
-        }
-        if (isset($identityList['identity_cookie_uuid']) && $identityList['identity_cookie_uuid']) {
-            $this->identityList['$identity_cookie_id'] = $identityList['identity_cookie_uuid'];
-        }
+        $this->getIdentityList($identityList);
+        // 如果传入的 $logPath 不为空，则使用传入的 $logPath；否则使用配置文件中的默认路径
+        $this->logPath = $logPath ?: config('sensors.log_path') . '/sensors.' . date('Y-m-d') . '.log';
+    }
 
-        return true;
+    /**
+     * 获取用户身份属性
+     * @param array $identityList
+     * @return array
+     */
+    public function getIdentityList(array $identityList): array
+    {
+        foreach ($identityList as $key => $value) {
+            switch ($key) {
+                case 'user_uuid':
+                    $this->identityList['$identity_login_id'] = $value;
+                    break;
+                case 'identity_mp_unionid':
+                    $this->identityList['$identity_mp_unionid'] = $value;
+                    break;
+                case 'identity_cookie_uuid':
+                    $this->identityList['$identity_cookie_id'] = $value;
+                    break;
+                default:
+                    // 如果不是上述三个键，则直接设置到 $this->identityList
+                    $this->identityList[$key] = $value;
+                    break;
+            }
+        }
+        return $this->identityList;
     }
 }
